8e23179a3db9e98a784a05035611925238dce8f5
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2025-12-24 15:24:53 +0530

    test_wideband_enterprise

--- tests/test_wideband_enterprise.py
+++ tests/test_wideband_enterprise.py
@@ -1,6 +1,7 @@
 import pytest
+from enterprise.signals.parameter import Uniform
 from enterprise_wideband.pulsar import WidebandPulsar
-from enterprise_wideband.signals import WidebandTimingModel
+from enterprise_wideband.signals import WidebandMeasurementNoise, WidebandTimingModel
 from pint.config import examplefile
 from pint.models import get_model_and_toas
 
@@ -26,3 +27,19 @@ def test_timing_model(psr: WidebandPulsar):
     assert len(tm_sig.params) == 0
     assert tm_sig.get_basis().shape[0] == len(psr.toas)
     assert tm_sig.get_basis().shape[1] == len(tm_sig.get_phiinv([]))
+
+
+def test_white_noise(psr: WidebandPulsar):
+    wn = WidebandMeasurementNoise(
+        log10_t2equad=Uniform(-8, -5), log10_dmequad=Uniform(-6, -3)
+    )
+    wn_sig = wn(psr)
+    assert wn_sig.name == f"{psr.name}_wideband_measurement_noise"
+    assert len(wn_sig.params) == 4
+    params = {
+        f"{psr.name}_efac": 1.0,
+        f"{psr.name}_log10_t2equad": -7.0,
+        f"{psr.name}_dmefac": 1.1,
+        f"{psr.name}_log10_dmequad": -4.5,
+    }
+    assert len(wn_sig.get_ndiag(params)) == len(psr.toas)
