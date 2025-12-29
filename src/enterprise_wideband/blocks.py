718dedcfcfc06ea8ba5d9c04875a31146cf006a4
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2025-12-29 13:54:18 +0530

    prior

--- src/enterprise_wideband/blocks.py
+++ src/enterprise_wideband/blocks.py
@@ -10,7 +10,7 @@ from .signals import (
 
 def achromatic_red_noise_powerlaw_block(
     log10_A: Parameter = Uniform(-18, -12),
-    gamma: Parameter = Uniform(0, 7),
+    gamma: Parameter = Uniform(1, 7),
     components: int = 30,
 ):
     spectrum = powerlaw(log10_A=log10_A, gamma=gamma)
@@ -20,7 +20,7 @@ def achromatic_red_noise_powerlaw_block(
 
 def dm_noise_powerlaw_block(
     log10_A: Parameter = Uniform(-18, -12),
-    gamma: Parameter = Uniform(0, 7),
+    gamma: Parameter = Uniform(1, 7),
     components: int = 30,
 ):
     spectrum = powerlaw(log10_A=log10_A, gamma=gamma)
